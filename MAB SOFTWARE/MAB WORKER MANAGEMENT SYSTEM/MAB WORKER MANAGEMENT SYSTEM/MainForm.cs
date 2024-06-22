ea2d378fbe2d729634674170cd8a27917097ca7a
Jad Chehade <chehadeh.jad1@gmail.com>
2024-06-22 12:12:02 +0300

    Intializing the project

---
+++ MAB SOFTWARE/MAB WORKER MANAGEMENT SYSTEM/MAB WORKER MANAGEMENT SYSTEM/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eng. moayad azab
+ * Date: 6/22/2024
+ * Time: 12:10 م
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MAB_WORKER_MANAGEMENT_SYSTEM
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ MAB SOFTWARE/MAB WORKER MANAGEMENT SYSTEM/MAB WORKER MANAGEMENT SYSTEM/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eng. moayad azab
+ * Date: 6/22/2024
+ * Time: 12:10 م
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace MAB_WORKER_MANAGEMENT_SYSTEM
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
